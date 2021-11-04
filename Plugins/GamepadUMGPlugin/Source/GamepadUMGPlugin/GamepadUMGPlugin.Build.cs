e1ed172d9e3af650757502b44d7bafc4ba1ac3bf
Weston Mitchell <westonmitchell@live.com>
2021-11-03 22:52:16 -0700

    gamepad cursor plugin

---
+++ Plugins/GamepadUMGPlugin/Source/GamepadUMGPlugin/GamepadUMGPlugin.Build.cs
@@ -0,0 +1,21 @@
+// Copyright 2015 by Nathan "Rama" Iyer. All Rights Reserved.
+using UnrealBuildTool;
+
+public class GamepadUMGPlugin : ModuleRules
+{
+    public GamepadUMGPlugin(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivatePCHHeaderFile = "Private/GamepadUMGPluginPrivatePCH.h";
+
+        PublicDependencyModuleNames.AddRange(
+			new string[] { 
+				"Core", 
+				"CoreUObject", 
+				"Engine", 
+				"InputCore"
+			}
+		);
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "UMG" });
+	}
+}
