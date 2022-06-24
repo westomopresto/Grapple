cae402a916b3d1819cb80f5439dc936d2b5a3a23
Weston Mitchell <westonmitchell@live.com>
2022-06-23 22:04:16 -0700

    Build Tools notes

---
+++ BUILD_TOOLS/FO_server.bat
@@ -0,0 +1,4 @@
+@echo on
+ssh weston@45.33.114.231 ~/updateFadeoutServers.sh
+ssh weston@139.162.146.38 ~/updateFadeoutServers.sh
+pause
---
+++ BUILD_TOOLS/updateFadeoutServers.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+pkill GrappleServer
+
+steamcmd +login anonymous +app_update 1380950 +quit
+nohup sh ~/.steam/steamapps/common/Fadeout\ Dedicated/GrappleServer.sh > /dev/null &
